cb252de9628ecc5c8737e7e2e39e90de897c1656
Corey Butler <corey@coreybutler.com>
2019-01-03 00:42:58 -0600

    Updated to support more manifest files.

--- README.md
+++ README.md
@@ -1,3 +1,81 @@
 # metadoc-api
 
-This [metadoc](https://github.com/author/metadoc) post-processor will generate a static (read-only) JSON API for browsing metadoc data.
+This [metadoc](https://github.com/author/metadoc) post-processor will generate a static (read-only) JSON "API" for browsing metadoc data.
+
+The output is structured in a simple folder that can be uploaded to a static web host. However; since there is **no** `index.html` file, it's important to configure your server to use `index.json` as the default file.
+
+The file structure will always contain:
+
+```
+/path/to/your/output
+├─┬ global
+│ ├── MyClassA.json
+│ ├── MyClassB.json
+│ └── …
+├─┬ mynamespace
+│ ├── MyClassC.json
+│ ├── MyClassD.json
+│ └── …
+├── api.bus.json
+├── api.classes.json
+├── api.json
+├── api.bus.json
+├── api.namespaces.json
+└── index.json
+```
+
+An example of the `index.json` (last file) may be:
+
+```json
+"full_spec_href": "https://domain.com/4.0.0/api.json",
+"bus_href": "https://domain.com/4.0.0/api.bus.json",
+"exceptions_href": "https://domain.com/4.0.0/api.exceptions.json",
+"class_href": "https://domain.com/4.0.0/api.classes.json",
+"namespace_href": "https://domain.com/4.0.0/api.namespaces.json"
+}
+```
+
+## Usage
+
+Simply run `metadoc-api` as a command in the metadoc chain. For example:
+
+```sh
+metadoc --source ./src --output ./docs | metadoc-api
+```
+
+There are two optional flags: `--root` (web root) and `--version` (API version/subfolder).
+
+```sh
+metadoc --source ./src --output ./docs | metadoc-api --root https://domain.com/ --version 2.0.0
+```
+
+The example above will produce links prefixed with `https://domain.com/2.0.0/`.
+
+**--version auto** can be used to automatically identify the version from a local `package.json` file. The source directory will be searched for a `package.json` file first, then the current working directory.
+
+## Guiding Principles
+
+This is a read-only/static API, but it attempts to follow reasonable API guidelines. For example, subresources are all identified with a `href` or `_href` attribute, allowing API clients to traverse the entire API programmatically.
+
+The API aims to be as simple as possible, but documentation websites are often more complex than they appear. To facilitate the simplest development flow possible, a few helper files are generated and put in the API root:
+
+**index.json**
+This file acts like `index.html` would in a web server, i.e. the default page served when no other is specified. It contains links to the primary resources documented in the API.
+
+**api.json**
+This is a failsafe file. It contains all of the output from metadoc, in case you can't get the detail you need from anywhere else. _WARNING:_ This file can be really, really big (10K+ lines). Use alternatives whenever possible.
+
+**api.bus.json**
+ Remember, metadoc was designed for [NGN](https://github.com/ngnjs/ngn), which is an object oriented programming library focused around a core event emitter. The bus is an event bus. This file contains all of the events triggered by an event bus. If you're not using NGN, this file may or may not be of use to you.
+
+ **api.classes.json**
+
+ This file contains a list of all available classes in the code being documented.
+
+ **api.exceptions.json**
+
+ This file contains all of the global/customer exceptions triggered by the code being documented.
+
+ **api.namespaces.com**
+
+ This file contains all of the namespaces recognized within the code being documented.
--- index.js
+++ index.js
@@ -7,7 +7,31 @@ class ApiGenerator extends MetadocPlugin {
     super(...arguments)
 
     this.NAME = require(path.join(__dirname, 'package.json')).name
-    this.APIROOT = '/'
+    this.APIROOT = this.getCLIArg('--root') || '/'
+
+    if (this.APIROOT.trim().substr(this.APIROOT.trim().length - 1, 1) !== '/') {
+      this.APIROOT = `${this.APIROOT}/`
+    }
+
+    let version = this.getCLIArg('--version') || null
+
+    if (version) {
+      if (version.toLowerCase() === 'auto') {
+        try {
+          version = JSON.parse(fs.readFileSync(path.join(this.source, 'package.json')).toString()).version
+        } catch (e) {
+          try {
+            version = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json')).toString()).version
+          } catch (ee) {
+            throw new Error(`Could not find package.json in "${this.source}" or "${process.cwd()}". Autodetection failed.`)
+            process.exit(1)
+          }
+        }
+      }
+
+      this.APIROOT = `${this.APIROOT}${version}/`
+      this.output = path.join(this.output, version)
+    }
   }
 
   get root () {
@@ -28,10 +52,21 @@ class ApiGenerator extends MetadocPlugin {
     let meta = Object.assign({}, this.SOURCE)
 
     meta.namespaces = Object.keys(meta.namespaces).map(ns => {
-      return `${this.APIROOT}/${ns}/index.json`.replace(/\/+/gi, '/')
+      return `${this.APIROOT}/${ns}/index.json`.replace(/\/+/gi, '/').replace(':/', '://')
     })
 
-    fs.writeFileSync(path.join(this.mkdirp(this.output), 'index.json'), JSON.stringify({ namespaces: meta.namespaces }, null, 2))
+    fs.writeFileSync(path.join(this.mkdirp(this.output), 'api.json'), JSON.stringify(this.data, null, 2))
+    fs.writeFileSync(path.join(this.mkdirp(this.output), 'api.classes.json'), JSON.stringify(this.classList, null, 2))
+    fs.writeFileSync(path.join(this.mkdirp(this.output), 'api.exceptions.json'), JSON.stringify(this.data.exceptions, null, 2))
+    fs.writeFileSync(path.join(this.mkdirp(this.output), 'api.bus.json'), JSON.stringify(this.data.bus, null, 2))
+    fs.writeFileSync(path.join(this.mkdirp(this.output), 'api.namespaces.json'), JSON.stringify(this.manifest, null, 2))
+    fs.writeFileSync(path.join(this.mkdirp(this.output), 'index.json'), JSON.stringify({
+      full_spec_href: `${this.APIROOT}api.json`,
+      bus_href: `${this.APIROOT}api.bus.json`,
+      exceptions_href: `${this.APIROOT}api.exceptions.json`,
+      class_href: `${this.APIROOT}api.classes.json`,
+      namespace_href: `${this.APIROOT}api.namespaces.json`
+    }, null, 2))
 
     this.emit('complete')
   }
@@ -83,6 +118,59 @@ class ApiGenerator extends MetadocPlugin {
     })
 
     meta.namespaces.forEach(subNamespace => this.processNamespace(subNamespace, data.namespaces[subNamespace], namespace.replace(/\/+/gi, '/')))
+  }
+
+  get classList () {
+    let data = {}
+
+    Object.keys(this.data.classes).forEach(cls => {
+      data[cls] = {
+        href: `${this.APIROOT}${cls.split('.').length === 1 ? 'global/' : ''}${cls.replace(/\./gi, '/')}.json`,
+        description: this.data.classes[cls].description
+      }
+    })
+
+    return data
+  }
+
+  // Generates a manifest/tree of all classes and namespaces based on the output of the process
+  get manifest () {
+    let data = this.data//JSON.parse(fs.readFileSync(path.join(this.output, 'api.json')))
+    let namespaces = Object.assign({}, data.namespaces)
+    let output = {}
+
+    Object.keys(namespaces).forEach(namespace => {
+      output = Object.assign(output, this.generateManifestNamespace(namespaces[namespace]))
+    })
+
+    return output
+  }
+
+  generateManifestNamespace (namespace, namespacePath = []) {
+    let data = {}
+
+    if (namespace) {
+      namespacePath.push(namespace.label)
+
+      data[namespace.label] = {
+        href: `${this.APIROOT}${namespacePath.join('/')}/index.json`,
+        description: namespace.description,
+        classes: namespace.classes.map(cls => {
+          return {
+            name: cls,
+            href: `${this.APIROOT}${namespacePath.join('/')}/${cls.replace(namespacePath.join('.') + '.', '')}.json`
+          }
+        }),
+        namespaces: {}
+      }
+
+      Object.keys(namespace.namespaces).forEach(ns => {
+        let subspace = this.generateManifestNamespace(namespace.namespaces[ns], namespacePath.slice(0))
+        data[namespace.label].namespaces = Object.assign(data[namespace.label].namespaces, subspace)
+      })
+    }
+
+    return data
   }
 }
 
